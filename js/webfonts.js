b6f2a7936072c328ac972b14afdec8bcd0a0636d
workmade <studio@workma.de>
2014-10-15 17:29:56 -0700

    add ga-id and small updates

--- js/webfonts.js
+++ js/webfonts.js
@@ -2,8 +2,40 @@
     google: {
       families:
       [
-      'Oswald:400',
-      'Source+Code+Pro:300,400'
+      // 'Abel:400',
+      // 'Bitter:400',
+      // 'Cousine:400,700',
+      // 'Cutive+Mono:400',
+      // 'Droid+Sans:400',
+      // 'Droid+Serif:400',
+      // 'Dosis:200,300,400,500,700,800',
+      // 'Fira+Mono:400,700',
+      // 'Fira+Sans:300,400,500,700',
+      // 'Inconsolata:400,700',
+      // 'Lato:100,300,400,700,900',
+      // 'Merriweather:300,400',
+      // 'Monda:400,700',
+      // 'Noticia+Text:400',
+      // 'Noto+Serif:400',
+      'Oswald:300,400,700',
+      // 'Open+Sans:300,400,600,700,800',
+      // 'Open+Sans+Condensed:300,700',
+      // 'Oxygen+Mono:400',
+      // 'Playfair+Display:400,700,900',
+      // 'PT+Sans:400,700',
+      // 'PT+Sans+Caption:400,700',
+      // 'PT+Serif:400,700',
+      // 'PT+Serif+Caption:400,700',
+      // 'PT+Mono:400',
+      // 'Roboto:100,300,400,500,700,900',
+      // 'Roboto+Condensed:300,400,700',
+      // 'Roboto+Slab:100,300,400',
+      // 'Signika:300,400,600,700',
+      // 'Source+Sans+Pro:200,300,400,600,700,900',
+      'Source+Code+Pro:200,300,400,600,700,900'
+      // 'Titillium+Web:200,300,400,600,700',
+
+
       ]
     }
   };
--- readme.md
+++ readme.md
@@ -1,2 +1,3 @@
 [![ ](/img/splash.jpg)](http://workmade.github.io/nansi)
-<a style="float:right;overflow:hidden;cursor:default" href="https://github.com/workmade/nansi"><img src="https://ga-beacon.appspot.com/UA-42808437-3/workmade/nansi?pixel" alt="analytics"/></a>
+<a style="float:right;overflow:hidden;cursor:default" href="https://github.com/workmade/nansi"><img src="https://ga-beacon.appspot.com/UA-42808437-2/workmade/nansi?pixel" alt="analytics"/></a>
+[readme.pdf](readme.pdf)
