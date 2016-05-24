81d98c55c45d84bbce8610973bf4524dd65fa4bf
Mike Warren <mkwr@workma.de>
2016-05-24 01:14:35 -0700

    updates/fixes

--- dist/nansi.sh
+++ dist/nansi.sh
@@ -1,99 +1,107 @@
 ## ⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼
-##  nansi.sh
-##
-##  site: mkwr.github.io/nansi
-##  github: https://github.com/mkwr/nansi
-##  author: Mike Warren
+## nansi.sh
+## mkwr.github.io/nansi
+## https://github.com/mkwr/nansi
+## © 2106 Mike Warren
+## License: MIT
 
-STOP=$(tput sgr0)  # reset
-BOLD=$(tput bold)  # bold
-UNDR=$(tput smul)  # underline
-RVRS=$(tput rev)   # reverse
-ITAL=$(tput sitm)  # italic [?]
+STOP=$(tput sgr0)    # reset
+BOLD=$(tput bold)    # bold
+UNDR=$(tput smul)    # underline
+RVRS=$(tput rev)     # reverse
+ITAL=$(tput sitm)    # italic [?]
+HIDE=$(tput civis)   # hide cursor
+SHOW=$(tput cnorm)   # show cursor
 
-declare -a fg=(
-  $(for i in {0..255}; do
-      echo F"$i"=$(tput setaf "$i")
-  done)
+# the number array (global)
+declare -ag NUMS=(
+   $(echo {0..255})
 )
 
-declare -a bg=(
-  $(for i in {0..255}; do
+# the foreground array
+declare -a F=(
+   $(for i in ${NUMS[@]}; do
+      echo F"$i"=$(tput setaf "$i")
+   done)
+)
+
+# the background array
+declare -a B=(
+   $(for i in ${NUMS[@]}; do
       echo B"$i"=$(tput setab "$i")
-  done)
+   done)
 )
 
 nansi () {
-  if [ -x /usr/bin/tput ]; then
-    for i in ${fg[@]}; do
-      export "$i"
-    done
-    for i in ${bg[@]}; do
-      export "$i"
-    done
-  fi
+   if [ -x /usr/bin/tput ]; then
+      for i in ${F[@]} ; do
+         export -n "$i"
+      done
+      for i in ${B[@]} ; do
+         export -n "$i"
+      done
+   fi
 }
 
 nansi
 unset nansi
 
-N16 ()	{
-  echo;for i in {0..15}; do
-    printf "\e[38;5;%sm %4s \e[0m" "${i}" "${i}"
-    if [ $((($i + 1) % 6)) == 0 ]; then
-      echo
-    fi
-  done
+N16 () {
+   echo
+   for i in ${NUMS[@]:0:16}; do
+      printf "\e[38;5;%sm %4s \e[0m" "${i}" "${i}"
+      if [ $((($i + 1) % 6)) == 0 ]; then
+         echo
+      fi
+   done
 }
-N216 ()	{
-  echo;for i in {16..231}; do
-    printf "\e[38;5;%sm %4s \e[0m" "${i}" "${i}"
-    if [ $((($i + 1) % 6)) == 4 ]; then
-      echo
-    fi
-  done
+N216 () {
+   echo
+   for i in ${NUMS[@]:16:216}; do
+      printf "\e[38;5;%sm %4s \e[0m" "${i}" "${i}"
+      if [ $((($i + 1) % 6)) == 4 ]; then
+         echo
+      fi
+   done
 }
-N24 ()	{
-  echo;for i in {232..255}; do
-    printf "\e[38;5;%sm %4s \e[0m" "${i}" "${i}"
-    if [ $((($i + 1) % 6)) == 4 ]; then
-      echo
-    fi
-  done
+N24 () {
+   echo
+   for i in ${NUMS[@]:232:24}; do
+      printf "\e[38;5;%sm %4s \e[0m" "${i}" "${i}"
+      if [ $((($i + 1) % 6)) == 4 ]; then
+         echo
+      fi
+   done
 }
 
 N256 () {
-  if [[ $1 == "-g" || $1 == "--grid" ]]; then
-    printf '%s\n'
-    printf '%s\n' "16 STANDARD COLORS"
-    N16
-    printf '%s\n'
-    printf '%s\n'
-    printf '%s\n' "6 x 6 x 6 COLORS"
-    N216
-    printf '%s\n'
-    printf '%s\n' "24 GREYSCALE COLORS"
-    N24
-  fi
+   # N256 prints multi-column no swatch
+   if [[ -z $1 ]]; then
+      printf '%s\n'
+      printf '%s\n' "16 STANDARD COLORS"
+      N16
+      printf '%s\n'
+      printf '%s\n'
+      printf '%s\n' "6 x 6 x 6 COLORS"
+      N216
+      printf '%s\n'
+      printf '%s\n' "24 GREYSCALE COLORS"
+      N24
+   fi
 
-  if [[ $1 == "-s" || $1 == "--swatch" ]]; then
-    printf '%s\n'
-    printf '%s\n' " 16 STANDARD COLORS"
-    printf '%s\n'
-    for i in {0..16}; do
-      printf '%s\n' "$(tput setaf "$i")▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ "$i"$(tput sgr0)"
-    done
-    printf '%s\n'
-    printf '%s\n' "  6 x 6 x 6 COLORS "
-    printf '%s\n'
-    for i in {17..231}; do
-      printf '%s\n' "$(tput setaf "$i")▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ "$i"$(tput sgr0)"
-    done
-    printf '%s\n'
-    printf '%s\n' "24 GREYSCALE COLORS"
-    printf '%s\n'
-    for i in {232..255}; do
-      printf '%s\n' "$(tput setaf "$i")▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ "$i"$(tput sgr0)"
-    done
-  fi
+   # N256 [-s or --swatch] prints single column + swatch
+   if [[ $1 == "-s" || $1 == "--swatch" ]]; then
+      printf '%s\n' ; printf '%s\n' " 16 STANDARD COLORS" ; printf '%s\n'
+      for i in ${NUMS[@]:0:16}; do
+         printf '%s\n' "$(tput setaf "$i")▓▓▓▓▓▓▓▓▓▓ "$i"$(tput sgr0)"
+      done
+      printf '%s\n' ; printf '%s\n' "  6 x 6 x 6 COLORS " ; printf '%s\n'
+      for i in ${NUMS[@]:16:216}; do
+         printf '%s\n' "$(tput setaf "$i")▓▓▓▓▓▓▓▓▓▓ "$i"$(tput sgr0)"
+      done
+      printf '%s\n' ; printf '%s\n' "24 GREYSCALE COLORS" ; printf '%s\n'
+      for i in ${NUMS[@]:232:24}; do
+         printf '%s\n' "$(tput setaf "$i")▓▓▓▓▓▓▓▓▓▓ "$i"$(tput sgr0)"
+      done
+   fi
 }
